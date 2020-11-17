d164acbcb91f8dd1ed58292e5c3b5b4122a97c54
Joakim Teixeira <setokim1@hotmail.com>
2020-11-17 06:05:47 -0300

    Add about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from 'pages/Home';
+import About from 'pages/About';
 
 const App = () => (
   <BrowserRouter>
-    <Route path="/">
+    <Route exact path="/">
       <Home />
+    </Route>
+
+    <Route path="/about">
+      <About />
     </Route>
   </BrowserRouter>
 );
---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <header>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam minima dolorem iste.</p>
+  </header>
+);
+
+export default About;
