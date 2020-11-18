779082204be67edf0e9d4ef4aba21995b991d830
Joakim Teixeira <setokim1@hotmail.com>
2020-11-18 09:26:59 -0300

    Add mobile responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,13 +10,16 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand as={Link} to="/">SlashDev</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link as={NavLink} to="/" exact>Home</Nav.Link>
-        <Nav.Link as={NavLink} to="/about">About</Nav.Link>
-        <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
-        <Nav.Link as={NavLink} to="/blog">Blog</Nav.Link>
-      </Nav>
-      <Button variant="outline-danger" onClick={handleAuth}>{isLoggedIn ? 'Logout' : 'Login'}</Button>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={NavLink} to="/" exact>Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+          <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
+          <Nav.Link as={NavLink} to="/blog">Blog</Nav.Link>
+        </Nav>
+        <Button variant="outline-danger" onClick={handleAuth}>{isLoggedIn ? 'Logout' : 'Login'}</Button>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
